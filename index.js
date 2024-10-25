16b8beb1e4e11f1f8a251715fbe447bad44cd465
pragyan <ghimirepragyan100@gmail.com>
2024-10-25 15:48:57 +0545

    html and css completed

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+app.get("/add",(req,res)=>{
+    res.render("add.ejs")
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`);
+});
